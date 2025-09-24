819339e1c2c71d45aca1aec175553e1d540487c2
RockosBos <32206608+RockosBos@users.noreply.github.com>
2025-09-23 22:01:22 -0400

    File Read

--- Assignment1/script.py
+++ Assignment1/script.py
@@ -1,5 +1,10 @@
 from git import Repo
 
+inputFile = open("./Input/VR_Project_List.txt")
+lines = inputFile.readlines()
+
+print(lines[3])
+
 currentRepoPath = "https://github.com/stefaanvermassen/virtual-museum-app.git"
 currentRepoName = currentRepoPath.split(".git")[0].split('/')[-1]
 
