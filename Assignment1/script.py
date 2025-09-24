ee488cb2775ff4f17475a0679e0f96859667831d
RockosBos <32206608+RockosBos@users.noreply.github.com>
2025-09-23 22:54:12 -0400

    File input and gitignore

--- Assignment1/script.py
+++ Assignment1/script.py
@@ -3,10 +3,13 @@ from git import Repo
 inputFile = open("./Input/VR_Project_List.txt")
 lines = inputFile.readlines()
 
-print(lines[3])
-
-currentRepoPath = "https://github.com/stefaanvermassen/virtual-museum-app.git"
-currentRepoName = currentRepoPath.split(".git")[0].split('/')[-1]
-
-Repo.clone_from(currentRepoPath, f"./Repos/{currentRepoName}/")
-print("Repo Cloned")
+for line in lines:
+    line = line.replace("\n", "")
+    currentRepoPath = line
+    currentRepoName = currentRepoPath.split(".git")[0].split('/')[-1]
+    
+    try:
+        Repo.clone_from(currentRepoPath, f"./Repos/{currentRepoName}/")
+        print(f"Cloned {currentRepoName}")
+    except:
+        print(f"{currentRepoName} Does not exist")
